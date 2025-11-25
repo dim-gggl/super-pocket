dd7ad83e0380821fcf6a8ceaec494d77d5ec73ad
dim-gggl <dimitri.gaggioli@gmail.com>
2025-11-25 01:53:00 +0100

    feat: add project init module structure

---
+++ super_pocket/project/init/__init__.py
@@ -0,0 +1,10 @@
+"""
+Project initialization module.
+
+Provides project scaffolding with template-based generation and
+interactive customization.
+"""
+
+from .engine import ProjectGenerator
+
+__all__ = ["ProjectGenerator"]
